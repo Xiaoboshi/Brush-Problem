b49e36df78532448e1d8daed3dbb797e27ae9fcf
Xiaoboshi <35205994+Xiaoboshi@users.noreply.github.com>
2020-03-16 09:55:55 +0800

    Create 1367.java

---
+++ leecode/1367/1367.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public boolean dfs(ListNode head, TreeNode root) {
+        if (head == null) {
+            return true;
+        }
+
+        if (root == null) {
+            return false;
+        }
+
+        if (head.val != root.val) {
+            return false;
+        }
+
+        return dfs(head.next, root.left) || dfs(head.next, root.right);
+    }
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) return false;
+        return (dfs(head, root) || isSubPath(head, root.left)
+                 || isSubPath(head, root.right));
+    }
+}
