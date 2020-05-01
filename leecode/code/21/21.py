6918533d1c05500a095fadb0043f75a20dbda401
Xiaoboshi <35205994+Xiaoboshi@users.noreply.github.com>
2020-05-01 10:18:26 +0800

    Create 21.py

---
+++ leecode/code/21/21.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        res = ListNode(0);
+        result = res
+        while (l1 != None) and (l2 != None):
+            if l1.val <= l2.val:
+                res.next = l1
+                l1 = l1.next
+            else:
+                res.next = l2
+                l2 = l2.next
+            res = res.next
+        
+        if l1 is None:
+            res.next = l2
+        if l2 is None:
+            res.next = l1
+
+        return result.next
