9efcea26aa14e05b275dcccaafffac43712073de
Xiaoboshi <35205994+Xiaoboshi@users.noreply.github.com>
2020-05-01 10:18:37 +0800

    Create 94.java

---
+++ leecode/code/94/94.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack();
+        List<Integer> result = new ArrayList();
+        if (root == null)
+            return result;
+        
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+}
